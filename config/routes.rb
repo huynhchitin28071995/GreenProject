9de54da952a0bdaf0de189e26bef904b75da4d38
huynhphi199700 <huynhphi199700@gmail.com>
2021-03-20 16:32:52 +0700

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
