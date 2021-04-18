a5cf416c3f81da943a0685dab1ae599fc44ec5d6
thanh <ubuntu@may5.localdomain>
2021-04-18 16:43:04 +0700

    18/4-products

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 
+import 'https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"'
 import NavbarTrigger from '../js/events/navbar_event'
 import '../stylesheets/application.scss'
 const images = require.context('../images',true)
