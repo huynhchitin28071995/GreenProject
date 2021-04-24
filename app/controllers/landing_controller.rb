2a1d6da01d9f0bfc88795aaf60e80601b0a5896d
Your Name <you@example.com>
2021-04-24 16:45:07 +0700

    product slider

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -9,62 +9,78 @@ class LandingController < ApplicationController
 
   def fetch_product
     @products = [
-      OpenStruct.new(
+
       {
         id: 1,
         name: 'thuoc a',
-        image: 'http://placeimg.com/300/480/arch/grayscale',
+        image: 'vitamins/vitamin1.jpg',
         nsx: 'Pharmacity',
         npp: 'Green Academy',
-        hot: true,
-        km: false,
-        discount: 0.1,
+        events: {
+          hot: true,
+          km: false,
+          discount: 0.1
+        },
+
         price: 100
-      }),
-      OpenStruct.new(
+      },
+
       {
         id: 2,
         name: 'thuoc b',
-        image: 'http://placeimg.com/300/480/arch/grayscale',
+        image: 'vitamins/vitamin2.jpg',
         nsx: 'Pharmacity',
         npp: 'Green Academy',
-        hot: true,
-        km: false, discount: 0.1,
+        events: {
+          hot: true,
+          km: true,
+          discount: 0.1
+        },
         price: 100
-      }),
-      OpenStruct.new(
+      },
+
       {
         id: 3,
         name: 'thuoc c',
-        image: 'http://placeimg.com/300/480/arch/grayscale',
+        image: 'vitamins/vitamin3.jpg',
         nsx: 'Pharmacity',
         npp: 'Green Academy',
-        hot: true,
-        km: false, discount: 0.1,
+        events: {
+          hot: true,
+          km: true,
+          discount: 0.1
+        },
         price: 100
-      }),
-      OpenStruct.new(
+      },
+
       {
         id: 4,
         name: 'thuoc d',
-        image: 'http://placeimg.com/300/480/arch/grayscale',
+        image: 'vitamins/vitamin4.jpg',
         nsx: 'Pharmacity',
         npp: 'Green Academy',
-        hot: true,
-        km: false, discount: 0.1,
+        events: {
+          hot: false,
+          km: false,
+          discount: 0
+        },
         price: 100
-      }),
-      OpenStruct.new(
+      },
+
       {
         id: 5,
         name: 'thuoc e',
-        image: 'http://placeimg.com/300/480/arch/grayscale',
+        image: 'vitamins/vitamin5.jpg',
         nsx: 'Pharmacity',
         npp: 'Green Academy',
-        hot: true,
-        km: false, discount: 0.1,
+        events: {
+          hot: false,
+          km: false,
+          discount: 0.1
+        },
         price: 100
-      })
+      }
     ]
+    @products = @products.map { |item| OpenStruct.new(item) }
   end
 end
