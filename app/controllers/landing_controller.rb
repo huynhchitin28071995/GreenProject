4499ddbd17a25254ab2440993e8e21435e76196c
thanh <ubuntu@may5.localdomain>
2021-04-24 16:46:00 +0700

    24/4 items

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -8,82 +8,99 @@ class LandingController < ApplicationController
 
     private
     def fetch_product
-        @product = {
+        @product = [
+            {
             id: 1,
             name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            discount:'10%',
+            events: {
+                hot: true,
+                gift:true,
+            }
         },
         {
             id: 2,
-            name:'thuoc abc_2', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            events: {
+                discount:'10%',
+                hot: true,
+                gift:true,
+            }
         },
         {
             id: 3,
-            name:'thuoc abc_3', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            events: {
+                discount:'10%',
+
+            }
         },
         {
             id: 4,
-            name:'thuoc abc_4', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            events: {
+                hot: true,
+                discount:'10%',
+            }
         },
         {
             id: 5,
-            name:'thuoc abc_5', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            discount:'10%',
+            events: {
+                hot: true,
+                gift:true,
+            }
         },
         {
             id: 6,
-            name:'thuoc abc_6', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            discount:'10%',
+            events: {
+                hot: true,
+                gift:true,
+            }
         },
         {
             id: 7,
-            name:'thuoc abc_7', 
+            name:'thuoc abc_1', 
             image: 'https://via.placeholder.com/300x150',
             nsx: 'Pharmacity', 
             npp: 'Green Academy',
-            hot: true,
-            km: false,
             price: 100,
-            discount:'10%'
+            discount:'10%',
+            events: {
+                hot: true,
+                gift:true,
+            }
         }
+    ]
+            @product = @product.map{ |item| OpenStruct.new(item)}
     end
+
+
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+    def discount(product)
+        product.price-(product.price * product.events[:discount].to_f/100.0).round(0)
+    end
 end
